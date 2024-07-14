bfd75fa53e58c493bb71ab06bb648443d48c3600
rectangle0123 <rectangle0123@gmail.com>
2024-07-14 11:22:21 +0900

    環境別ビルド設定

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,15 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // dart-defineをInfo.plistから取得する
+    let dartDefinesString = Bundle.main.infoDictionary!["DartDefines"] as! String
+    var dartDefines = [String:String]()
+    for definedValue in dartDefinesString.components(separatedBy: ",") {
+      let decoded = String(data: Data(base64Encoded: definedValue)!, encoding: .utf8)!
+      let values = decoded.components(separatedBy: "=")
+      dartDefines[values[0]] = values[1]
+    }
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
